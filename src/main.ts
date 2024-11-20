08e0671a7531c7301826b302762ae475d3d24a1a
Block_2222 <WebBrewDev@proton.me>
2024-11-19 19:56:00 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/*
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	Project Name
+	Project Description
+	
+	Copyright DATE Organization
+    Licenced under LICENCE
+	~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    GPL text (or whatever licence youre using) goes here
+*/
